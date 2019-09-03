7e6d760415f9c5a512615bf4e32ac5a367aa4971
Gabriel Lopes Marques <gabriellopesmarques@hotmail.com>
2019-09-03 19:53:40 -0300

    add twitter meta tags

--- core/DocumentObjectModel.php
+++ core/DocumentObjectModel.php
@@ -63,7 +63,28 @@ class DocumentObjectModel
             "og:site_name",
             "og:locale",
             "og:url",
-            "fb:app_id"
+            "fb:app_id",
+            "twitter:card",
+            "twitter:site",
+            "twitter:site:id",
+            "twitter:creator",
+            "twitter:creator:id",
+            "twitter:description",
+            "twitter:title",
+            "twitter:image",
+            "twitter:image:alt",
+            "twitter:player",
+            "twitter:player:width",
+            "twitter:player:height",
+            "twitter:player:stream",
+            "twitter:app:name:iphone",
+            "twitter:app:url:iphone",
+            "twitter:app:name:ipad",
+            "twitter:app:id:ipad",
+            "twitter:app:url:ipad",
+            "twitter:app:name:googleplay",
+            "twitter:app:id:googleplay",
+            "twitter:app:url:googleplay"
         ];
 
         $metas = $this->content->getElementsByTagName("meta");
